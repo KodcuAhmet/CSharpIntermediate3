ae25ecff9016f0712a73f7e2900680928d95f343
KodcuAhmet <ahmet.cigil@tuta.com>
2024-09-27 16:42:53 +0200

    Dictionary example done

--- CSharpIntermediate3/Program.cs
+++ CSharpIntermediate3/Program.cs
@@ -1,11 +1,42 @@
-﻿namespace CSharpIntermediate3
+﻿using System.Diagnostics.Tracing;
+
+namespace CSharpIntermediate3
 {
     internal class Program
     {
-        class
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Dictionary<string, string> words = new Dictionary<string, string>();
+            while (true)
+            {
+                Console.WriteLine("------------------");
+                Console.WriteLine("Add <word>");
+                Console.WriteLine("Define <word>");
+                Console.WriteLine("------------------");
+
+                var input = Console.ReadLine();
+                var parts = input.Split(" ");
+
+                if (parts.Length != 2)
+                    continue;
+
+                if (parts[0] == "add")
+                {
+                    Console.Write("Define {0}: ", parts[1]);
+                    var definition = Console.ReadLine();
+                    words.Add(parts[1], definition);
+                }
+                else if (parts[0] == "define")
+                {
+                    string definition;
+                    if (!words.TryGetValue(parts[1], out definition))
+                    {
+                        Console.WriteLine("I don't know what a {0} is", parts[1]);
+                    }
+                    Console.WriteLine("{0} = {1}", parts[1], words[parts[1]]);
+                }
+            }
         }
     }
 }
